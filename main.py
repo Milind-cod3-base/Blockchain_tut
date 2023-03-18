d275f3aaa7792989bd6731f65bf42beddb81453e
Milind <milindchaudhary80@gmail.com>
2023-03-18 11:18:21 +0100

    Implement the functions

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Block:
         self.hash = self.calculate_hash()
 
     # function to calculate hash of given function
-    def caculate_hash(self):
+    def calculate_hash(self):
 
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return hashlib.sha256(block_string.encode()).hexdigest()
@@ -52,21 +52,63 @@ class Blockchain:
         return self.chain[-1]
     
     # add a new block into the chain
-    def add_block(self):
-        pass
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
 
         
     
     # check the validity/corruption of chain
     def is_chain_valid(self):
-        pass
+        for i in range(1, len(self.chain)):
+            # i starts from 1 as we dont need to check genesis block
+            current_block = self.chain[i]
 
+            previous_block = self.chain[i-1]
+
+            # check if the hash has changed of the block
+            if current_block.hash == current_block.calculate_hash():
+                b = "valid"
+                return b
+                #print(b)
+            
+            # double verifying the hash of previous block
+            # by checking hash stored in current block and 
+            # hash stored in previous block 
+            if current_block.previous_hash == previous_block.hash:
+                a = "valid"
+                return a
+                #print(a)
 
 
 #print("all ok")
 
 if __name__ == "__main__":
     
-    pass
+    # create 1st chain
+    blockchain1 = Blockchain()
+
+    # add first block
+    b1 = Block(1, time(),"")
+
+    blockchain1.add_block(b1)
+
+
+    # add second block
+    b2 = Block(2, time(),"")
+
+    blockchain1.add_block(b2)
+
+
     # below code is to check the attributes of a block
     #print(Block.__dict__)
+
+    #blockchain1.is_chain_valid
+    
+    # get the details of desired
+    print(blockchain1.chain[0].__dict__)
+
+    print(blockchain1.chain[1].__dict__)
+
+    print(blockchain1.chain[2].__dict__)

3d482147421b4d8ffefe53a82acbc4b37b706d40
Milind <milindchaudhary80@gmail.com>
2023-03-18 08:07:13 +0100

    classes created, ready to be pushed

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import hashlib
 import json
 from time import time
 
+
+# class to hold the data of each block
+class Block:
+    pass
+
+# class to add and validate the blocks in a chain
+class Blockchain:
+    pass
 #print("all ok")
+
+if __name__ == "__main__":
+    pass
